7db0bef1d41391ead2083597fb42f64fde232009
Vamshi Krishna Kotla <vkotla@webmd.net>
2019-11-22 16:53:09 +0530

    start

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
