7352916bd53787a1d83a7672018c78f8aeae9175
vamshi-krishna-kotla <kotlavamshi999@gmail.com>
2019-11-27 17:22:30 +0530

    Added basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const app = require('express')();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', ( req, res ) => {
+	res.end(`<h1>Working</h1>`);
+});
+
+app.listen( PORT, () => {
+	console.log(`Server running at port: ${PORT}`);
+});
