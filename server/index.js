8d63225197d8f369fca91e24ef4f2bdc65adce32
vamshi-krishna-kotla <kotlavamshi999@gmail.com>
2019-11-28 20:20:10 +0530

    Added '/songs' route to send static data

--- server/index.js
+++ server/index.js
@@ -2,8 +2,44 @@ const app = require('express')();
 
 const PORT = process.env.PORT || 3000;
 
+const songList = {
+	list : [
+		{
+			'song': 'bohemian rhapsody',
+			'artist': 'Queen',
+			'link': 'https://www.youtube.com/embed/fJ9rUzIMcZQ'
+		},
+		{
+			'song': 'believer',
+			'artist': 'Imagine Dragons',
+			'link': 'https://www.youtube.com/embed/7wtfhZwyrcc'
+		},
+		{
+			'song': 'thunder',
+			'artist': 'Imagine Dragons',
+			'link': 'https://www.youtube.com/embed/fKopy74weus'
+		},
+		{
+			'song': 'we will rock you',
+			'artist': 'Queen',
+			'link': 'https://www.youtube.com/embed/-tJYN-eG1zk'
+		},
+	]
+};
+
 app.get('/', ( req, res ) => {
-	res.end(`<h1>Working</h1>`);
+	res.send(`<h1>Working</h1>`);
+	res.end();
+});
+
+app.get('/songs', (req,res) => {
+	if(Object.entries(songList).length !== 0) {
+		res.status(200).end(JSON.stringify(songList));
+	}
+	else {
+		res.send(`<h2>No Data!</h2>`);
+		res.end();
+	}
 });
 
 app.listen( PORT, () => {
